6a1c7deedd058f83960e1d08bbfd994db8cae3d0
WebGenious <kavitarandive91@gmail.com>
2025-04-01 15:56:46 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './Pages/Header/Header';
+
+import Nomatch from './Pages/noMatch/Nomatch';
+import PostUser from './Pages/User/PostUser';
+import Datasaved from './Pages/Datasave/Datasaved';
+import Dashboard from './Pages/dashboard/Dashboard';
+import UpdateUser from './Pages/User/UpdateUser';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <Header></Header>
+   <Routes>
+   <Route path='/' element={<PostUser></PostUser>}>
+   </Route>
+   <Route path="/postuser" element={<PostUser></PostUser>}></Route>
+
+   {/* <Route path="/postuser/:id" element={<UpdateUser></UpdateUser>}></Route> */}
+   <Route path="/postuser/:id" element={<UpdateUser />} />
+
+  
+   <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+    {/* <Route path='/Employee' element={<PostUser />} /> */}
+    
+    <Route path="/data-saved" element={<Datasaved></Datasaved>} />
+    <Route path='*' element={<Nomatch></Nomatch>}>
+    </Route>
+   </Routes>
+   
+   </>
   );
 }
 
